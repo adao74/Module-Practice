a0255842b8d456d41aec0622cd4170f82c30a886
adao74 <andreaminghweidao@hotmail.com>
2024-08-10 13:10:00 -0500

    add main.mjs for ESM syntax

---
+++ main.mjs
@@ -0,0 +1,37 @@
+// ES Module (ESM) syntax
+// use import not require
+// file is .mjs not .js
+// In terminal: if `node main.mjs` doesn't work, do `node --experimental-modules main.mjs`
+
+import _ from 'lodash'
+
+const catchError = (res) => {
+
+    // For errors returned from the ENDPOINT (e.g. fetch todo that doesn't exist) (i.e. doesn't work if the error is due to endpoint typo!!!)
+    if(!res.ok) {
+        console.log("There's an error")
+        throw Error(res.statusText)  // res.statusText is passed into catch as the argument 
+    } 
+    
+    
+    return res.json()
+}
+
+
+const getPosts = () => {
+    fetch('http://jsonplaceholder.typicode.com/users')
+    .then( (response) => catchError(response))
+    .then(users => {
+
+        users = users.map(user => _.camelCase(user.name))
+
+        const chunked = _.chunk(users, 3)
+        console.log(chunked)
+    })
+    .catch(err => console.log(`Error,  ${err}`)) // for all errors
+  }
+
+getPosts()
+
+
+
